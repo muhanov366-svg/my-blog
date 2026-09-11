8cd7425fbf77633981d7ed12d7e29ee4d7187d02
muhanov366-svg <muhanov366@gmail.com>
2026-09-11 14:28:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ⚠️ Вставь сюда URL своего Web App (из Apps Script)
-const API_URL = 'https://script.google.com/macros/s/AKfycbwzqZICG-6LV-EmnTEXK2S1E7WyHIceAwJU4VQ9oiU_NgVC0IyeXZLM9qOe81-Pq2-_/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwIQ_PX9mz0H4wU-9WqDCCAqYF4As5Q5pvOnD5Zzyp875wJpSi-1rUAN-7MkDpwZG_i/exec';
 
 let currentUser = null; // {id, login, role, canPost, canComment}
 let cachedPosts = [];
